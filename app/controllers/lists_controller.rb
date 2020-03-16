106590c20f2c92b3311f02b44978bb817bf11c37
Harish Ramesh <harishramesh@Harishs-MacBook-Pro.local>
2020-03-16 19:32:08 +1100

    Added edit & delete

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -17,9 +17,25 @@ class ListsController < ApplicationController
       redirect_to list_path(list.id)
     end
 
+    def edit
+    @list = List.find params[:id]
+    end
+
+    def update
+      list = List.find params[:id]
+      list.update list_params
+      redirect_to lists_path(params[:id])
+    end
+
     def show
     @list = List.find params[:id]
     end
+
+    def destroy
+    list = List.find params[:id]
+    list.destroy
+    redirect_to lists_path
+  end
 
     private
     def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'lists/edit'
   root :to => 'pages#home'
   resources :users, :only => [:new, :create, :index]
 
